a8f2844848bd5e77376719511a99c0758fa09053
Blake Lucas <92932337+blake-lucas@users.noreply.github.com>
2023-10-05 09:25:27 -0500

    fix: retry firmware download up to 3 times

--- rocky-firmware.sh
+++ rocky-firmware.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 pip install --prefix=/usr bs4
 pip install --prefix=/usr requests
-python3 /tmp/download-firmware.py
-#If downloaded firmware is less than 100mb throw an error and quit bash
+
+for i in {1..3}; do
+    python3 /tmp/download-firmware.py
+
+    # If downloaded firmware is less than 100mb, retry after 15 seconds
+    if [ $(stat -c%s "/tmp/linux-firmware.rpm") -lt 100000000 ]; then \
+        echo "Error: File size of /tmp/linux-firmware.rpm is less than 100MB. Retrying in 15 seconds..."
+        sleep 15
+    else
+        break
+    fi
+done
+
+# If the download still fails after 3 attempts, throw an error and quit bash
 if [ $(stat -c%s "/tmp/linux-firmware.rpm") -lt 100000000 ]; then \
-    echo "Error: File size of /tmp/linux-firmware.rpm is less than 100MB. The download likely failed or something else is wrong."
+    echo "Error: File size of /tmp/linux-firmware.rpm is less than 100MB after 3 attempts. The download likely failed or something else is wrong."
     exit 1
 fi
+
 mkdir /tmp/rocky-firmware
 rpm2cpio /tmp/linux-firmware.rpm | cpio -idmv -D /tmp/rocky-firmware
 mv -f /tmp/rocky-firmware/usr/lib/firmware/* /lib/firmware/
